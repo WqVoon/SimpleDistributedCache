2ce5a8814fb791a08ca3d9539db884ab444e16b8
WqVoon <2027195958@qq.com>
2021-05-02 15:24:58 +0800

    实现了一致性哈希算法，并进行了测试

---
+++ consistent_hash.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type Hash func(data []byte) uint32
+
+type Map struct {
+	// hash 函数
+	hash Hash
+	// 记录一个真实节点对应几个虚拟节点
+	replicas int
+	// 哈希环
+	keys []int
+	// 用于存储虚拟节点与真实节点的映射，键是虚拟节点的哈希，值是真实节点的名称
+	hashMap map[int]string
+}
+
+func makeMap(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+func (self *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < self.replicas; i++ {
+			hash := int(self.hash([]byte(strconv.Itoa(i) + key)))
+			self.keys = append(self.keys, hash)
+			self.hashMap[hash] = key
+		}
+	}
+	sort.Ints(self.keys)
+}
+
+func (self *Map) Get(key string) string {
+	// 表示当前哈希环上没有任何数据，直接返回
+	if len(self.keys) == 0 {
+		return ""
+	}
+	// 根据提供的键来计算相应的哈希
+	hash := int(self.hash([]byte(key)))
+	// 在哈希环中查找第一个大于等于上面哈希值的下标
+	idx := sort.Search(len(self.keys), func(i int) bool {
+		return self.keys[i] >= hash
+	}) % len(self.keys)
+	// 返回相应的节点名，取余是为了当 idx == len(self.keys) 时令其为 0
+	return self.hashMap[self.keys[idx]]
+}
--- main.go
+++ main.go
@@ -3,29 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"strconv"
 )
 
-var db = map[string]string{
-	"Name":  "PsyDuck",
-	"Age":   "21",
-	"Hobby": "Program",
-}
-
 func main() {
-	MakeGroup("info", 20, GetterFunc(
-		func(key string) ([]byte, error) {
-			log.Println("Search key", key, "from db")
-			time.Sleep(1 * time.Second)
-			if v, ok := db[key]; ok {
-				return []byte(v), nil
-			}
-			return nil, fmt.Errorf("%s not exist", key)
-		},
-	))
+	hash := makeMap(3, func(data []byte) uint32 {
+		ret, _ := strconv.Atoi(string(data))
+		return uint32(ret)
+	})
 
-	addr := "localhost:8080"
-	log.Println("Serve at", addr)
-	http.ListenAndServe(addr, makeHTTPPool(addr))
+	// 这时哈希环应该上增加了虚拟节点 2, 4, 12, 14, 22, 24
+	hash.Add("4", "2")
+	if fmt.Sprintln(hash.keys) != "[2 4 12 14 22 24]\n" {
+		log.Fatal("Add func error")
+	}
+
+	testCases := map[string]string{
+		"2":  "2",
+		"11": "2",
+		"23": "4",
+		"27": "2",
+	}
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			log.Fatal("Get func err: ", k, "->", v)
+		}
+	}
+
+	// 这里加入了 8，哈希环中应该加入了 8, 18, 28，同时 27 应该映射到 8
+	hash.Add("8")
+	if fmt.Sprintln(hash.keys) != "[2 4 8 12 14 18 22 24 28]\n" {
+		log.Fatal("Add func error")
+	}
+	if hash.Get("27") != "8" {
+		log.Fatal("Get func err: 27->8")
+	}
+
+	log.Println("All passed")
 }
